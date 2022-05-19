9ead58ab498be66c7a73f38c26df893f898b65aa
Thiago Chiqueti <tchiqueti@gmail.com>
2022-05-19 11:25:39 -0300

    javaScript add

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,49 @@
-const firstAlert = "Olá, seja bem vindo ao Rest & Compile! \n Ainda estamos contruindo essa aplicação"
-alert(firstAlert)
+class product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+}
+
+function showMenu() {
+    let chosenOption
+    while(chosenOption != "1" && chosenOption != "2" && chosenOption != "3") {
+        chosenOption = prompt(  "O que deseja fazer?\n" +
+                                "1- Cadastrar produto\n" +
+                                "2- Imprimir produtos\n" +
+                                "3- Sair do programa")
+    }
+    return chosenOption
+}
+
+function createProduct() {
+    let productName = prompt("Informe o nome do produto: ")
+    let productPrice = prompt("Informe o valor do produto: ")
+    let products = new product(productName, productPrice)
+    return products
+}
+
+function printProductList(product) {
+    let productList = ""
+    product.forEach((product, index) => {
+        productList +=    (index + 1) + "- " + product.name +
+                            " (" + product.price + " R$)\n"
+    })
+    alert(productList)
+}
+
+let dataBase = []
+let chosenOption
+
+while(chosenOption != "3") {
+    chosenOption = showMenu()
+    switch(chosenOption) {
+        case "1":
+            let prodToAdd = createProduct()
+            dataBase.push(prodToAdd)
+            break
+        case "2":
+            printProductList(dataBase)
+            break
+    }
+}
